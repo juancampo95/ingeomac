69b2851c399919e9259e561fd98f1f9740edcd97
juancampo95 <juandesigner95@gmail.com>
2018-06-21 18:41:46 -0500

    cambios en index completamente para iniciar de nuevo

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Ingeomac Ltda</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/mdb.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav></nav>
+
+	<header class="container-fluid header" style="background-image: url('images/Inicio.jpg');">
+		<h1>Calidad y compromiso</h1>
+	</header>
+
+	<section class="container-fluid">
+		<article class="container callToAction">
+			<h2>Servicios Web</h2>
+			<p>Aquí en Ingeomac LTDA siempre pensamos en nuestros clientes y como mejorar la atención y los servicios ofrecidos. Es por esta razón que renovamos nuestro sitio web y aprovechamos la nuevas tecnologías para hacerla dinámica y funcional. Brindando dos la consulta de informes de manera online y la actualización de los datos personales para facilitar el contácto.</p>
+			<div>
+				<a class="btn btn-danger btn-lg" href="#">Consultar Informes</a>
+				<a class="btn btn-grey btn-lg" href="#">Actualizar Datos</a>
+			</div>
+		</article>
+	</section>
+	<section class="container-fluid section-rojo">
+		<article class="container callToAction">
+			<h2>Servicios Web</h2>
+			<div class="row p-container">
+				<div class=" col-xs-12 col-sm-12 col-md-4 parrafos">			
+					<p>Contamos con 15 años de experiencia en el mercado.</p>
+				</div>
+				<div class=" col-xs-12 col-sm-12 col-md-4 parrafos">			
+					<p>Le permitimos conocer con exactitud la calidad de sus materiales.</p>
+				</div>
+				<div class=" col-xs-12 col-sm-12 col-md-4 parrafos">			
+					<p>Trabajamos con concreto, pavimento, mampostería, entre otros.</p>
+				</div>
+			</div>
+		</article>
+	</section>
+</body>
+</html>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/mdb.min.js"></script>
