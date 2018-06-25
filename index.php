5206b48f158a5018ec47ddf5fdf6b72b1be53a15
Juan Camilo <juandesigner95@gmail.com>
2018-06-25 12:49:16 -0500

    para merge

--- index.php
+++ index.php
@@ -116,6 +116,10 @@
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1991.3527431121433!2d-76.52335613849583!3d3.421739562852225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6c1007d3bbd%3A0x39365a968d1753d7!2sCl.+19+%2335-33%2C+Cali%2C+Valle+del+Cauca!5e0!3m2!1ses-419!2sco!4v1529944117981" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen></iframe>
 	</section>
 
+
+
+
+
 	<footer class="container-fluid pie_d_page section-rojo" style="background-color:#b20a0a;">
         <div class="row">
             <div class="col-md-4 col-xs-12">
