e78305f656db12f549891e22c90d8e3fc2b8a9e2
Juan Camilo <juandesigner95@gmail.com>
2018-06-25 12:38:14 -0500

    cambios de servicios

---
+++ components/cards_index.php
@@ -0,0 +1,49 @@
+<section class="container-fluid">
+  <article class="container callToAction">
+    <h2>Algunos de nuestros servicios</h2>
+    <div class="row mb-5 cards_container">
+      <!-- Card -->
+      <div class="card col-md-3 m-3">
+        <div class="view overlay">
+          <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">Card title</h4>
+          <a href="#" class="btn btn-danger">Button</a>
+        </div>
+      </div>
+      <!-- Card -->
+      <!-- Card -->
+      <div class="card col-md-3 m-3">
+        <div class="view overlay">
+          <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">Card title</h4>
+          <a href="#" class="btn btn-danger">Button</a>
+        </div>
+      </div>
+      <!-- Card -->
+      <!-- Card -->
+      <div class="card col-md-3 m-3">
+        <div class="view overlay">
+          <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">Card title</h4>
+          <a href="#" class="btn btn-danger">Button</a>
+        </div>
+      </div>
+      <!-- Card -->
+    <div>
+  </article>
+</section>
