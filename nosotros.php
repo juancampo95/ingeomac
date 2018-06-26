2dbbc517e0bfc985f3bbcdfcba479c8daed161e2
Jitrainos <alejandrocs789@hotmail.com>
2018-06-26 10:25:25 -0500

    Nosotros

--- nosotros.php
+++ nosotros.php
@@ -1,6 +1,6 @@
 <?php include("nav.php") ?>
 <header class="container-fluid header" style="background-image: url('images/QuienesSomosBackground.jpg');">
   <div class="container">
-    <h1>Calidad y compromiso</h1>
+    
   </div>
 </header>
