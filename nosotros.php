21783107b0a638df01f384e366028d659cad5af0
Jitrainos <alejandrocs789@hotmail.com>
2018-06-26 13:07:43 -0500

    nosotros cambios

--- nosotros.php
+++ nosotros.php
@@ -2,10 +2,21 @@
 <header class="container-fluid header us-header" style="background-image: url('images/Nosotros.jpg');">
   <div class="container callToAction section-rojo flotante">
     <h2>¿Quiénes Somos?</h2>
-    <div class="row">
-      <div class="col-xs-12 col-md-6"></div>
-      <div class="col-xs-12 col-md-6"></div>
+    <div class="row us-inicio">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <img src="images/Mujer.jpg" alt="">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <p>Somos un laboratorio de concretos, suelos y pavimentos, dedicado al control de calidad de los materiales usados en las construcciones de obras civiles y la patología de estructuras, comprometidos con la satisfacción del cliente entregando resultados oportunos, idóneos y confiables, dando cumplimiento a las normas vigentes en la realizaciones de cada uno de nuestros ensayos, con equipos modernos e infraestructura que nos hacen lÍderes a nivel nacional.</p>
+      </div>
     </div>
+      <div class="col-xs-12 col-md-12 redes-somos">
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/facebookwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/whatsappwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/instagramwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/twitterwhite.svg"></a>
+      </div>
+
 
   </div>
 </header>
