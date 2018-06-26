16c1f03976d2cd36f4a27fccc007c369776ef649
Juan Camilo <juandesigner95@gmail.com>
2018-06-26 11:26:17 -0500

    Merge branch 'master' of https://github.com/juancampo95/ingeomac

--- nosotros.php
+++ nosotros.php
@@ -1,6 +1,11 @@
 <?php include("nav.php") ?>
-<header class="container-fluid header" style="background-image: url('images/Nosotros.jpg');">
-  <div class="container callToAction section-rojo">
+<header class="container-fluid header us-header" style="background-image: url('images/Nosotros.jpg');">
+  <div class="container callToAction section-rojo flotante">
+    <h2>¿Quiénes Somos?</h2>
+    <div class="row">
+      <div class="col-xs-12 col-md-6"></div>
+      <div class="col-xs-12 col-md-6"></div>
+    </div>
 
   </div>
 </header>
