4391dec421d6594ee0844376d77c423eebacde36
Jitrainos <alejandrocs789@hotmail.com>
2018-06-29 11:30:37 -0500

    Pagina Contacto

---
+++ contacto.php
@@ -0,0 +1,56 @@
+<?php include("nav.php") ?>
+
+<header class="container-fluid header us-header" style="background-image: url('images/Contacto.jpg');">
+  <div class="container callToAction section-blanco flotante barra">
+    <h2 class="textobarra">Contactenos</h2>
+    <div class="row us-inicio">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <img src="images/Mujer.jpg" alt="">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <p>Somos un laboratorio de concretos, suelos y pavimentos, dedicado al control de calidad de los materiales usados en las construcciones de obras civiles y la patología de estructuras, comprometidos con la satisfacción del cliente entregando resultados oportunos, idóneos y confiables, dando cumplimiento a las normas vigentes en la realizaciones de cada uno de nuestros ensayos, con equipos modernos e infraestructura que nos hacen lÍderes a nivel nacional.</p>
+      </div>
+    </div>
+      <div class="col-xs-12 col-md-12 redes-somos">
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/facebookwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/whatsappwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/instagramwhite.svg"></a>
+        <a target="new_blank" href="https://www.facebook.com/etniaropadeportiva/"><img src="images/rrss/twitterwhite.svg"></a>
+      </div>
+  </div>
+</header>
+<div class="red-space"></div>
+<div class="container-fluid section-rojo">
+  <div class="container section-rojo info-contacto">
+    <h2>Datos de Contacto</h2>
+    <p>En INGEOMAC LTDA estamos comprometidos con la prestación de un servicio de calidad, es por eso que estamos siempre atentos a cualquier tipo de comunicación que nuestros clientes quieran tener con nosotros, debido a eso tenemos distintos canales y medios por los cuales nos podemos  comunicar.</p>
+  </div>
+  <article class="container callToAction section-rojo ">
+    <div class="row p-container">
+      <div class=" col-xs-12 col-sm-12 col-md-4 parrafos-contacto parrafosrojo-contacto">
+        <img src="images/direccionblanco.svg"></a>
+        <h2>Dirección</h2>
+        <p>Cl. 19 #35 - 33, Cali, Valle del Cauca</p>
+      </div>
+      <div class=" col-xs-12 col-sm-12 col-md-4 parrafos-contacto parrafosrojo-contacto">
+        <img src="images/telefonoblanco.svg"></a>
+        <h2>Teléfono</h2>
+        <p>+57 (2) 373 3707</p>
+        <p>+57 315 527 5724</p>
+      </div>
+      <div class=" col-xs-12 col-sm-12 col-md-4 parrafos-contacto parrafosrojo-contacto">
+        <img src="images/emailblanco.svg"></a>
+        <h2>Email</h2>
+        <p>Escríbenos</p>
+        <p>gerencia@ingeomac.com</p>
+      </div>
+    </div>
+  </article>
+</div>
+
+
+
+
+
+
+<?php include("footer.php") ?>
