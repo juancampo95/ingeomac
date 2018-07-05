cd06efc09d8b25f5dba4dd5807a42e5dcf963e0f
Jitrainos <alejandrocs789@hotmail.com>
2018-07-05 11:26:15 -0500

    Cambios

--- servicios_info.php
+++ servicios_info.php
@@ -1,19 +1,22 @@
 <!-- CONCRETO Y MORTERO -->
 <div id="s1" class="info_container">
 <h1>Compresión de cilindros de concreto y flexión de vigas</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>El objetivo principal del ensayo consiste en determinar la máxima resistencia a la compresión de un cilindro de muestra de un concreto frente a una carga aplicada axialmente. Las mezclas de concreto (Hormigón) se pueden diseñar de tal manera que tengan una amplia variedad de propiedades mecánicas y de durabilidad que cumplan con los requerimientos de diseño de la estructura. La resistencia a la compresión del concreto es la medida más común de desempeño que emplean los ingenieros para diseñar edificios y otras estructuras.</p>
+<p>La resistencia a la compresión se mide fracturando probetas cilíndricas de concreto en una maquina de ensayo de compresión. La resistencia a la compresión se calcula a partir de la carga de ruptura dividida por el área de la sección que resiste a la carga y se soporta en unidades de libra-fuerza por pulgada cuadrada (psi). Los resultados de las pruebas de resistencia a la compresión se emplean fundamentalmente para determinar que la mezcla de concreto suministrada cumpla con los requerimientos de la resistencia especificada.</p>
+<p>Los resultados de las pruebas de resistencia a partir de cilindros fundidos se pueden utilizar para fines de control de calidad, aceptación del concreto o para estimar la resistencia del concreto en estructuras para programar las operaciones de construcción. Un resultado de prueba es el promedio de por lo menos 2 pruebas de resistencia curadas de manera estándar o convencional elaboradas con la misma muestra de concreto y sometidas a ensayo a la misma edad. Para la mayoría de los casos, los requerimientos de resistencia para el concreto se alcanzan a la edad de 28 días.</p>
+<p>Para la realización de este ensayo, nuestro laboratorio sigue los lineamientos expresados en la norma NTC 673.
+Tomado de la norma NTC 673</p>
 <div class="row div_img_container">
-  <div class="imagenes_divs" style="background-image:url('')"></div>
-  <div class="imagenes_divs" style="background-image:url('')"></div>
-  <div class="imagenes_divs" style="background-image:url('')"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/175.jpg')"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/182.png'); background-repeat:no-repeat;background-position:center; background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/mortero.jpg'); background-size:cover;"></div>
 </div>
 <h2 class="titulo_tabla">Servicios que ofrecemos en concretos y morteros</h2>
 <table class="table table-condensed table-bordered">
   <thead>
     <tr>
-      <th>CONCRETO</th>
-      <th>MORTERO</th>
+      <th style="font-family:Caviar-Dreams-Bold;">CONCRETO</th>
+      <th style="font-family:Caviar-Dreams-Bold;">MORTERO</th>
     </tr>
   </thead>
   <tbody>
@@ -23,7 +26,43 @@
     </tr>
     <tr>
       <td>Una resistencia (Incluye propiedades físicas completas)</td>
-      <td>Una resistencia (Incluye propiedades físicas completas)</td>
+      <td>Cada resistencia adicional</td>
+    </tr>
+    <tr>
+      <td>Una resistencia (Sin propiedades físicas)</td>
+      <td>Resistencia a la compresión simple de cilindros (3"x6") o cubos</td>
+    </tr>
+    <tr>
+      <td>Cada resistencia adicional</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Verificación de diseño de mezcla</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión simple de cilindros con curado normal</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión simple de cilindros con curado acelerado</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Resistencia a la flexión de vigas</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Tracción indirecta (Ensayo Brasilero)</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Densidad del concreto (Medida sobre cilindros)</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Evaluacion esclerometrica</td>
+      <td></td>
     </tr>
   </tbody>
 </table>
@@ -32,17 +71,54 @@
 <!-- Mampostería estructural -->
 <div id="s2" class="info_container">
 <h1>Mampostería estructural</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>El sistema de mampostería reforzada se fundamenta en la construcción de muros con piezas de mampostería de perforación vertical, unidas por medio de mortero, reforzadas internamente con barras y alambres de acero, cumpliendo los requisitos de análisis, diseño y construcción apropiados establecidos en la NSR - 2010. </p>
+<p>Este sistema permite la inyección de todas sus celdas con mortero de relleno o inyectar sólo las celdas verticales que llevan refuerzo.</p>
+<p>La construcción se realiza por medio de procedimientos y actividades tradicionales de mampostería, aunque los muros pueden prefabricarse formando paneles. Su unidad de medida es el metro cuadrado.</p>
 <div class="row div_img_container">
-  <div class="imagenes_divs"></div>
-  <div class="imagenes_divs"></div>
-  <div class="imagenes_divs"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/213.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/214.jpg')"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/215.jpg')"></div>
 </div>
+<h2 class="titulo_tabla">Servicios que ofrecemos en mampostería estructural</h2>
 <table class="table table-condensed table-bordered">
   <thead>
+    <tr>
+      <th style="font-family:Caviar-Dreams-Bold;">MAMPOSTERÍA</th>
+    </tr>
   </thead>
   <tbody>
+    <tr>
+      <td>Absorción por inmersión de ladrillos o bloques</td>
+    </tr>
+    <tr>
+      <td>Absorción por ebullición de ladrillos o bloques</td>
+    </tr>
+    <tr>
+      <td>Densidad de bloques o ladrillos</td>
+    </tr>
+    <tr>
+      <td>Rata inicial de absorción de ladrillos</td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión de ladrillos o bloques</td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión de Muretes</td>
+    </tr>
+    <tr>
+      <td>Resistencia a la flexión de ladrillos -adoquines y Losetas</td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión de ladrillos o bloques</td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión de ladrillos o bloques</td>
+    </tr>
+    <tr>
+      <td>Resistencia a compresión de ladrillos o bloques</td>
+    </tr>
   </tbody>
 </table>
 </div>
+
+<!-- Mezcla Aslfaltica -->
