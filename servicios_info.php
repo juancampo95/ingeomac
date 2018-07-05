5b6e13c5fc18e9d6f2034babe0dea6ba343a699f
Jitrainos <alejandrocs789@hotmail.com>
2018-07-05 12:16:12 -0500

    Añadiendo servicios

--- servicios_info.php
+++ servicios_info.php
@@ -12,7 +12,7 @@ Tomado de la norma NTC 673</p>
   <div class="imagenes_divs" style="background-image:url('images/productos/mortero.jpg'); background-size:cover;"></div>
 </div>
 <h2 class="titulo_tabla">Servicios que ofrecemos en concretos y morteros</h2>
-<table class="table table-condensed table-bordered">
+<table class="table table-condensed">
   <thead>
     <tr>
       <th style="font-family:Caviar-Dreams-Bold;">CONCRETO</th>
@@ -80,7 +80,7 @@ Tomado de la norma NTC 673</p>
   <div class="imagenes_divs" style="background-image:url('images/productos/215.jpg')"></div>
 </div>
 <h2 class="titulo_tabla">Servicios que ofrecemos en mampostería estructural</h2>
-<table class="table table-condensed table-bordered">
+<table class="table table-condensed">
   <thead>
     <tr>
       <th style="font-family:Caviar-Dreams-Bold;">MAMPOSTERÍA</th>
@@ -109,16 +109,163 @@ Tomado de la norma NTC 673</p>
       <td>Resistencia a la flexión de ladrillos -adoquines y Losetas</td>
     </tr>
     <tr>
-      <td>Resistencia a compresión de ladrillos o bloques</td>
+      <td>Propiedades Dimensionales</td>
     </tr>
     <tr>
-      <td>Resistencia a compresión de ladrillos o bloques</td>
+      <td>Porción de muro extraído en obra</td>
     </tr>
     <tr>
-      <td>Resistencia a compresión de ladrillos o bloques</td>
+      <td>Control dimensional</td>
     </tr>
   </tbody>
 </table>
 </div>
 
 <!-- Mezcla Aslfaltica -->
+
+<div id="s3" class="info_container">
+<h1>Mezcla Aslfaltica</h1>
+<p>En una mezcla asfáltica en caliente de pavimentación, el asfalto y el agregado son combinados en proporciones exactas: Las proporciones relativas de estos materiales determinan las propiedades físicas de la mezcla y, eventualmente, el desempeño de la misma como pavimento terminado. Existen dos métodos de diseño comúnmente utilizados para determinar las proporciones apropiadas de asfalto y agregado en una mezcla. Ellos son el método Marshall y el Método Hveem. Ambos métodos de diseño son ampliamente usados en el diseño de mezclas asfálticas de pavimentación. La selección y uso de cualquiera de estos métodos de diseño de mezclas es, principalmente, asuntos de gustos en ingeniería, debido a que cada método contiene características y ventajas singulares. Cualquier método pude ser usado con resultados satisfactorios. </p>
+<p>Una muestra de mezcla de pavimentación preparada en el laboratorio puede ser analizada para determinar su posible desempeño en la estructura del pavimento. El análisis está enfocado hacia cuatro características de la mezcla, y la influencia que estas puedan tener en el comportamiento de la mezcla. Las cuatro características son: </p>
+<p>1. Densidad de la mezcla</p>
+<p>2. Vacíos de aire, o simplemente vacíos.</p>
+<p>3. Vacíos en el agregado mineral.</p>
+<p>4. Contenido de asfalto.</p>
+<div class="row div_img_container">
+  <div class="imagenes_divs" style="background-image:url('images/productos/231.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/232.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/965.jpg'); background-size:cover;"></div>
+</div>
+<h2 class="titulo_tabla">Servicios que ofrecemos en mezcla asfáltica</h2>
+<table class="table table-condensed">
+  <thead>
+    <tr>
+      <th style="font-family:Caviar-Dreams-Bold;">MEZCLAS ASFÁLTICAS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Toma de briquetas</td>
+    </tr>
+    <tr>
+      <td>Contenido de asfaltos</td>
+    </tr>
+    <tr>
+      <td>Densidad de pavimento</td>
+    </tr>
+    <tr>
+      <td>Estabilidad y flujo (resistencia)</td>
+    </tr>
+    <tr>
+      <td>Granulometria agregado</td>
+    </tr>
+    <tr>
+      <td>Espesores de carpeta asfáltica</td>
+    </tr>
+    <tr>
+      <td>Galletas (extracción con cincel)</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+<!-- Suelos -->
+
+<div id="s4" class="info_container">
+<h1>Suelos</h1>
+<p>Los ensayos geotécnicos de laboratorio son pruebas realizadas para la determinación de las características geotécnicas de un terreno. Estos ensayos se ejecutan sobre las muestras previamente obtenidas en el lugar del terreno y, dependiendo del tipo de ensayo, se exigen distintas calidades de muestra.</p>
+<p>Para la determinación de las propiedades del suelo, los ensayos se clasifican en:</p>
+<p><span class="negrita">Ensayos de identificación:</span> son los únicos (junto con los de compactación) que pueden realizarse sobre muestras alteradas. Pueden ser:</p>
+<p>- Físicos: granulometría, plasticidad o peso específico de partículas.</p>
+<p>- Químicos: contenido en sulfatos, carbonatos o materia orgánica.</p>
+<p><span class="negrita">Ensayos de estado:</span> humedad natural, peso específico seco o aparente. Proporcionan la situación del terreno en su estado natural. Como excepción, pueden utilizarse muestras alteradas para la obtención de la humedad natural, siempre que se protejan de pérdidas posteriores de humedad nada más proceder a su obtención.</p>
+<p><span class="negrita">Ensayos de permeabilidad:</span> en permeámetros de carga constante, de carga variable o en célula triaxial.</p>
+<p><span class="negrita">Ensayos de cambio de volumen:</span> compresibilidad edométrica, expansividad (presión de hinchamiento, hinchamiento libre, índice de Lambe) y colapso.</p>
+<p>Ensayos de resistencia: compresión simple, corte directo (CD, CU, UU), compresión triaxial (CD, CU, UU).</p>
+<p><span class="negrita">Otros ensayos sobre suelos o rocas:</span></p>
+<p>Compactación Próctor</p>
+<p>Índice de dispersividad Pin-Hole (sobre muestra alterada).</p>
+<p><span class="negrita">Ensayos sobre rocas:</span> compresión simple (con o sin galgas extensométricas), carga puntual (Point Load), corte directo de diaclasas, índice de durabilidad Slake, compresión triaxial.</p>
+<p><span class="negrita">Ensayos químicos sobre agua freática:</span> obtención de pH, de contenido en sales solubles o de elementos contaminantes.</p>
+<div class="row div_img_container">
+  <div class="imagenes_divs" style="background-image:url('images/productos/291.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/290.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/suelos.jpg'); background-size:cover;"></div>
+</div>
+<h2 class="titulo_tabla">Servicios que ofrecemos en suelos</h2>
+<table class="table table-condensed">
+  <thead>
+    <tr>
+      <th style="font-family:Caviar-Dreams-Bold;">SUELOS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Contenido de humedad</td>
+    </tr>
+    <tr>
+      <td>Densidad de campo</td>
+    </tr>
+    <tr>
+      <td>Cada punto adicional</td>
+    </tr>
+    <tr>
+      <td>Límites de Atterberg</td>
+    </tr>
+    <tr>
+      <td>Próctor modificado con peso específico</td>
+    </tr>
+    <tr>
+      <td>CBR compactado en laboratorio</td>
+    </tr>
+    <tr>
+      <td>CBR tomado en sitio</td>
+    </tr>
+    <tr>
+      <td>CBR (9 puntos)</td>
+    </tr>
+    <tr>
+      <td>Apiques hasta 60 cm</td>
+    </tr>
+    <tr>
+      <td>Apiques 70 cm- 150cm</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+<!-- Ensayos Quimicos -->
+
+<div id="s5" class="info_container">
+<h1>Ensayos Químicos</h1>
+<p>El <span class="negrita">Análisis Químico</span> es la técnica que se emplea para determinar la composición química de un material, es decir, nos da información sobre los diferentes elementos presentes en la muestra analizada. De manera que es posible determinar componentes mayoritarios, minoritarios y/o trazas de elementos en diferentes tipos de materiales.</p>
+<p>Estos ensayos son:</p>
+<p>Determinación del contenido de sales solubles en suelos.</p>
+<p>Determinación de pH en suelos</p>
+<p>Método de ensayo para determinar la solidez (Sanidad)</p>
+<p>Estabilización y modificación con cal</p>
+<p>Contenido de carbonato</p>
+<div class="row div_img_container">
+  <div class="imagenes_divs" style="background-image:url('images/productos/100.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/101.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/102.jpg'); background-size:cover;"></div>
+</div>
+</div>
+
+<!-- Patologia de Estructuras -->
+
+<div id="s6" class="info_container">
+<h1>Ensayos Químicos</h1>
+<p>El <span class="negrita">Análisis Químico</span> es la técnica que se emplea para determinar la composición química de un material, es decir, nos da información sobre los diferentes elementos presentes en la muestra analizada. De manera que es posible determinar componentes mayoritarios, minoritarios y/o trazas de elementos en diferentes tipos de materiales.</p>
+<p>Estos ensayos son:</p>
+<p>Determinación del contenido de sales solubles en suelos.</p>
+<p>Determinación de pH en suelos</p>
+<p>Método de ensayo para determinar la solidez (Sanidad)</p>
+<p>Estabilización y modificación con cal</p>
+<p>Contenido de carbonato</p>
+<div class="row div_img_container">
+  <div class="imagenes_divs" style="background-image:url('images/productos/100.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/101.jpg'); background-size:cover;"></div>
+  <div class="imagenes_divs" style="background-image:url('images/productos/102.jpg'); background-size:cover;"></div>
+</div>
+</div>
