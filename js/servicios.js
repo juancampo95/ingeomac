d2f3972ff57f5189afe0fc315259a54d935816a7
Juan Camilo <juandesigner95@gmail.com>
2018-07-05 12:19:13 -0500

    m

--- js/servicios.js
+++ js/servicios.js
@@ -1,7 +1,7 @@
   $(document).ready(function(){
     $("#s1").fadeIn();
     mostrar = function(contenedor){
-      for (var i = 1; i <= 2; i++) {
+      for (var i = 1; i <= 6; i++) {
         var n = "#s" + parseInt(i);
         $(n).hide();
       }
