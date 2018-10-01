2c61c81800c4a656bf6838ce1c8cfca308117a05
Juan Camilo <juandesigner95@gmail.com>
2018-10-01 13:53:06 -0500

    cambios en mostrarclientes.php

--- php/mostrarClientes.php
+++ php/mostrarClientes.php
@@ -2,11 +2,22 @@
 	require("conexion.php");
 
 	class mostrar extends conexion{
-
 		public function __construct($tabla){
 			parent::__construct();
 
-			
+			$sale = array();
+			$query  = "SELECT * FROM $tabla";
+			$result = mysqli_query($this->conexion,$query);
+
+			if(mysqli_num_rows($result)>0){
+				while ($row = mysqli_fetch_array($result)){
+					$sale[] = $row;
+					
+				}
+				echo json_encode($sale);
+			}else{
+				echo "alert('no funciono')";
+			}
 		}
 	}
 
